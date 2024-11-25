5dd290fbe6405d658039bc87ed10b2d386c2f2ca
Julia Bengert <yubengert@gmail.com>
2024-11-25 19:52:59 +0300

    add post request for contacts

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def home_page(request):
@@ -9,3 +10,8 @@ def home_page(request):
 def contacts(request):
     if request.method == "GET":
         return render(request, "catalog/contacts.html")
+    name = request.POST.get("name")
+    email = request.POST.get("email")
+    message = request.POST.get("message")
+    return HttpResponse(f"Спасибо, {name}!\n Мы получили ваше сообщение {message}\n"
+                        f"Ответ возможно придет на почту: {email}.")
