b639fb52861a53cdd7b7d249a00e944bb7906dd5
Julia Bengert <yubengert@gmail.com>
2024-12-06 09:21:14 +0300

    add page transitions

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("catalog.urls"))
+    path("", include("catalog.urls", namespace="catalog"))
 ]
 
 if settings.DEBUG:
