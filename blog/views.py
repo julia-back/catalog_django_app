3afc0a62816961e0c969610a9fafd2503c207040
Julia Bengert <yubengert@gmail.com>
2024-12-15 00:06:44 +0300

    add CRUD for blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import BlogPost
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class BlogListView(ListView):
+    model = BlogPost
+
+
+class BlogDetailView(DetailView):
+    model = BlogPost
+
+
+class BlogCreateView(CreateView):
+    model = BlogPost
+    fields = ["title", "content", "preview_img"]
+    success_url = reverse_lazy("blog:blog_list")
+
+
+class BlogUpdateView(UpdateView):
+    model = BlogPost
+    fields = ["title", "content", "preview_img"]
+    success_url = reverse_lazy("blog:blog_list")
+
+
+class BlogDeleteView(DeleteView):
+    model = BlogPost
+    template_name = "blog/blogpost_confirm_delete.html"
+    success_url = reverse_lazy("blog:blog_list")
