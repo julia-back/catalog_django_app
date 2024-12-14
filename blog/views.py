df63737726a9b4cc7394bbc7466e497605d27338
Julia Bengert <yubengert@gmail.com>
2024-12-15 01:02:18 +0300

    add redirect for update, filter is_published, count_views

--- blog/views.py
+++ blog/views.py
@@ -7,9 +7,19 @@ from django.urls import reverse_lazy
 class BlogListView(ListView):
     model = BlogPost
 
+    def get_queryset(self):
+        queryset = super().get_queryset().filter(is_published=True)
+        return queryset
+
 
 class BlogDetailView(DetailView):
     model = BlogPost
+
+    def get_object(self, queryset=None):
+        object_ = super().get_object(queryset)
+        object_.count_views += 1
+        object_.save()
+        return object_
 
 
 class BlogCreateView(CreateView):
@@ -22,6 +32,9 @@ class BlogUpdateView(UpdateView):
     model = BlogPost
     fields = ["title", "content", "preview_img"]
     success_url = reverse_lazy("blog:blog_list")
+
+    def get_success_url(self):
+        return reverse_lazy("blog:blog_detail", args=[self.kwargs.get("pk")])
 
 
 class BlogDeleteView(DeleteView):
