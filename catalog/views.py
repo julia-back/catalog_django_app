6863d9963bdbf4875ef04125ea0051879bb64280
Julia Bengert <yubengert@gmail.com>
2024-12-31 20:19:32 +0300

    add CRUD

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Product, Contacts
+from django.urls import reverse_lazy
+from .forms import ProductForm
 
 
 class ProductListView(ListView):
@@ -14,12 +16,30 @@ class ProductDetailView(DetailView):
     model = Product
 
 
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy("catalog:home")
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy("catalog:home")
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = "catalog/product_delete_form.html"
+    success_url = reverse_lazy("catalog:home")
+
+
 class ContactsView(View):
 
     def get(self, request):
         contacts = Contacts.objects.all()
         context = {"object_list": contacts}
-        return render(request, "catalog/contacts_list.html", context=context)
+        return render(request, "catalog/contacts.html", context=context)
 
     def post(self, request):
         name = request.POST.get("name")
