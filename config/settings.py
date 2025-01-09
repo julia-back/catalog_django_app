23d02993e3455bf62da5949e66b1e59e20a62afe
Julia Bengert <yubengert@gmail.com>
2025-01-09 21:43:56 +0300

    add model User in app users

--- config/settings.py
+++ config/settings.py
@@ -105,3 +105,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    email = models.CharField(max_length=25,
+                             unique=True,
+                             verbose_name="Email",
+                             help_text="Введите email. Он будет"
+                                       " использован для авторизации.")
+    avatar = models.ImageField(upload_to="users/avatars/",
+                               verbose_name="Фото профиля",
+                               blank=True,
+                               null=True)
+    phone_number = models.CharField(max_length=15,
+                                    verbose_name="Телефон",
+                                    help_text="Введите номер телефона",
+                                    blank=True,
+                                    null=True)
+    country = models.CharField(max_length=50,
+                               verbose_name="Страна",
+                               help_text="Введите страну проживания",
+                               blank=True,
+                               null=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", ]
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
